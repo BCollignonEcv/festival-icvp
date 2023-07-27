4a3fb549f17f7869331840622a05aa927cd15d74
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2023-07-27 02:00:57 +0200

    deploy

---
+++ assets/p_contact-f20c05ef.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as c,e as s,w as n,f as a}from"./index-d638d17d.js";const _={components:{}},r=a("h2",null,"Page contact",-1);function p(i,f,d,l,m,u){const e=t("c_section");return c(),s(e,{_size:100},{default:n(()=>[r]),_:1})}const x=o(_,[["render",p]]);export{x as default};
